e248b8be56d07afeaed2ccbed5e6469700d81960
NadyaPramuditaOnGit <154657389+NadyaPramuditaOnGit@users.noreply.github.com>
2025-08-31 20:26:29 +0800

    new commit today

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'desa_adat_cengkilung_db',
+  port: parseInt(process.env.DB_PORT) || 3306
+};
+
+const pool = mysql.createPool(dbConfig);
+
+module.exports = {pool};
